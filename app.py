e4a2d8463a1c7667a5ffccc508447cf8086bbb88
Carlos Bernal <ramirez.bernal.jesus1@gmail.com>
2022-09-29 00:06:48 -0700

    bernalgas: not found page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from werkzeug.utils import redirect
+from flask_mysqldb import MySQL
 import secrets
 import json
 app = Flask(__name__)
@@ -11,10 +12,16 @@ app.secret_key = secrets.token_hex(16)
 def index(): #index o root o home page
     return render_template("index.html")
 
+# not founded page
 
 @app.route('/item',methods=['GET','POST'])
 def item(): #item page
     return render_template('item.html',error=None)
+
+
+@app.errorhandler(404)
+def page_not_found(errorhandler):
+    return render_template("notfound.html"), 404
 
 if __name__ == "__main__":
     app.run(debug =True, port=8000 )
